fc37edcb2354716416f1c5e03d0fd2aa4b4c954a
MUSTHAFA786G <musthafa.sidvi@gmail.com>
2026-03-16 16:16:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
-	"github.com/MUSTHAFA786G/devsetup/cmd/devsetup"
+	"github.com/MUSTHAFA786G/devsetup/internal/analyzer"
+	"github.com/MUSTHAFA786G/devsetup/internal/cloner"
+	"github.com/MUSTHAFA786G/devsetup/internal/detector"
+	"github.com/MUSTHAFA786G/devsetup/internal/installer"
+	"github.com/MUSTHAFA786G/devsetup/internal/logger"
+	"github.com/MUSTHAFA786G/devsetup/internal/runner"
 )
 
 func main() {
